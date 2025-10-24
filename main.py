574439b5051496057f3453d6a8cffbe01e7fd000
ErofeevMaxal <erofeevmaxal@gmail.com>
2025-10-24 18:51:22 +0300

    board_selection_failure_fixed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from checkers.constants import WIDTH, HEIGHT, BOX_SIZE, SQUARE_SIZE
+from typing import Tuple
+from checkers.constants import WIDTH, HEIGHT, BOX_SIZE, SQUARE_SIZE, ROWS, COLS
 from checkers.board import Board
 from checkers.game import Game
 
@@ -7,11 +8,13 @@ WINDOW = pygame.display.set_mode((WIDTH + BOX_SIZE * 2, HEIGHT + BOX_SIZE * 2))
 pygame.display.set_caption('Сheckers')
 pygame.font.init()
 
-def get_row_col_from_mouse(pos):
+def get_row_col_from_mouse(pos: tuple[int, int]) -> tuple[int, int] | None:
     x, y = pos
     row = (y - BOX_SIZE) // SQUARE_SIZE
     col = (x - BOX_SIZE) // SQUARE_SIZE
-    return row, col
+    if 0 <= row < ROWS and 0 <= col < COLS:
+        return row, col
+    return None
 
 def main():
     game = Game(WINDOW)
@@ -25,7 +28,11 @@ def main():
             
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
-                row, col = get_row_col_from_mouse(pos)
+                point = get_row_col_from_mouse(pos)
+                if not point:
+                    continue
+                
+                row, col = point
                 game.select(row, col)
         
         game.update()
